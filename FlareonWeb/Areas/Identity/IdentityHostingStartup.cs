05567916be5958ee6cfd91f8469574f37d632ff7
harakiriboy <yesirkepov03@gmail.com>
2022-04-16 00:46:43 +0600

    first commit

---
+++ FlareonWeb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FlareonWeb.Areas.Identity.Data;
+using FlareonWeb.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FlareonWeb.Areas.Identity.IdentityHostingStartup))]
+namespace FlareonWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<FlareonWebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("FlareonWebContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+                    })
+                    .AddEntityFrameworkStores<FlareonWebContext>();
+            });
+        }
+    }
+}
